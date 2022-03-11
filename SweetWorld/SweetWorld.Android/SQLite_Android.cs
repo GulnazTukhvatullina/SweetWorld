140d0fc3feba6254bebb34e61514d42e2e9f33b6
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-03-11 10:54:48 +0300

    Installed sql lite

---
+++ SweetWorld/SweetWorld.Android/SQLite_Android.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SweetWorld.Droid;
+using System.IO;
+using Xamarin.Forms;
+using SweetWorld.SQLite;
+
+namespace SweetWorld.Droid
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLite_Android() { }
+        public string GetDatabasePath(string sqliteFilename)
+        {
+            string documentsPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            return path;
+        }
+    }
+}
