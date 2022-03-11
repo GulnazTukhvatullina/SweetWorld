a1c90ca5608a5d615dc186565070f968c4d97b4f
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-03-11 10:44:47 +0300

    Installed material

---
+++ SweetWorld/SweetWorld/SQLite/TablesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SweetWorld.SQLite
+{
+    public class TablesRepository
+    {
+        SQLiteConnection database;
+        public TablesRepository(string databasePath)
+        {
+            database = new SQLiteConnection(databasePath);
+        }
+    }
+}
