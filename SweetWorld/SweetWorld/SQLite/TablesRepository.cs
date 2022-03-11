58801077d47f2d934ac033b3aba15837da1d28b1
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-03-11 10:59:44 +0300

    Add class users

--- SweetWorld/SweetWorld/SQLite/TablesRepository.cs
+++ SweetWorld/SweetWorld/SQLite/TablesRepository.cs
@@ -11,6 +11,35 @@ namespace SweetWorld.SQLite
         public TablesRepository(string databasePath)
         {
             database = new SQLiteConnection(databasePath);
+            database.CreateTable<User>();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return database.Table<User>().ToList();
+        }
+
+        public User GetUser(int id)
+        {
+            return database.Get<User>(id);
+        }
+
+        public int DeleteUser(int id)
+        {
+            return database.Delete<User>(id);
+        }
+
+        public int SaveUser(User item)
+        {
+            if (item.Id != 0)
+            {
+                database.Update(item);
+                return item.Id;
+            }
+            else
+            {
+                return database.Insert(item);
+            }
         }
     }
 }
---
+++ SweetWorld/SweetWorld/SQLite/User.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SweetWorld.SQLite
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        [Unique]
+        public string Login { get; set; }
+        public string Password { get; set; }        
+        public string Name { get; set; }
+    }
+}
