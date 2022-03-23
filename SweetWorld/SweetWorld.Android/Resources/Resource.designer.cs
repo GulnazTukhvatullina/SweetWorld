5331ce255ab05211160e46b8405c75464d847b62
gulnaz.tukhvatullina.02@inbox.ru <gulnaz.tukhvatullina.02@inbox.ru>
2022-03-23 16:11:52 +0300

    Editing icons for the admin

--- SweetWorld/SweetWorld.Android/Resources/Resource.designer.cs
+++ SweetWorld/SweetWorld.Android/Resources/Resource.designer.cs
@@ -15330,49 +15330,49 @@ namespace SweetWorld.Droid
 			public const int call = 2131165287;
 			
 			// aapt resource value: 0x7F070068
-			public const int design_bottom_navigation_item_background = 2131165288;
+			public const int contacts = 2131165288;
 			
 			// aapt resource value: 0x7F070069
-			public const int design_fab_background = 2131165289;
+			public const int design_bottom_navigation_item_background = 2131165289;
 			
 			// aapt resource value: 0x7F07006A
-			public const int design_ic_visibility = 2131165290;
+			public const int design_fab_background = 2131165290;
 			
 			// aapt resource value: 0x7F07006B
-			public const int design_ic_visibility_off = 2131165291;
+			public const int design_ic_visibility = 2131165291;
 			
 			// aapt resource value: 0x7F07006C
-			public const int design_password_eye = 2131165292;
+			public const int design_ic_visibility_off = 2131165292;
 			
 			// aapt resource value: 0x7F07006D
-			public const int design_snackbar_background = 2131165293;
+			public const int design_password_eye = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int exitIcon = 2131165294;
+			public const int design_snackbar_background = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
-			public const int geo = 2131165295;
+			public const int exitIcon = 2131165295;
 			
 			// aapt resource value: 0x7F070070
-			public const int glavnaya = 2131165296;
+			public const int geo = 2131165296;
 			
-			// aapt resource value: 0x7F070075
-			public const int icon_about = 2131165301;
+			// aapt resource value: 0x7F070071
+			public const int glavnaya = 2131165297;
 			
 			// aapt resource value: 0x7F070076
 			public const int icon_feed = 2131165302;
 			
-			// aapt resource value: 0x7F070071
-			public const int ic_mtrl_checked_circle = 2131165297;
-			
 			// aapt resource value: 0x7F070072
-			public const int ic_mtrl_chip_checked_black = 2131165298;
+			public const int ic_mtrl_checked_circle = 2131165298;
 			
 			// aapt resource value: 0x7F070073
-			public const int ic_mtrl_chip_checked_circle = 2131165299;
+			public const int ic_mtrl_chip_checked_black = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int ic_mtrl_chip_close_circle = 2131165300;
+			public const int ic_mtrl_chip_checked_circle = 2131165300;
+			
+			// aapt resource value: 0x7F070075
+			public const int ic_mtrl_chip_close_circle = 2131165301;
 			
 			// aapt resource value: 0x7F070077
 			public const int mainIcon = 2131165303;
